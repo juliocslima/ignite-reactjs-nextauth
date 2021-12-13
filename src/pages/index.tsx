3b40805016c5f7ba8f49dd4c5df10d939778cb6b
Julio Lima <lima.julio.cs@gmail.com>
2021-12-13 17:45:48 -0300

    Login page created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import { Box, Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Input } from "../components/Form/Input";
 import { Logo } from "../components/Header/Logo";
 
+import { useAuth } from "../contexts/AuthContext";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState;
+  const { signIn } = useAuth();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await signIn(values);
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -20,19 +37,41 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Box align="center" mb="8">
           <Logo />
         </Box>
         
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input 
+            name="email" 
+            id="email"
+            type="email" 
+            label="E-mail" 
+            error={errors.email}
+            {...register("email")}
+          />
+
+          <Input 
+            name="password" 
+            id="password"
+            type="password" 
+            error={errors.password}
+            label="Senha" 
+            {...register("password")}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
-      
     </Flex>
   )
 }
