f502b861bf1de0437956d3096af384282f53c292
Julio Lima <lima.julio.cs@gmail.com>
2021-12-13 15:36:38 -0300

    Login page created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
-import type { NextPage } from 'next'
+import { Box, Button, Flex, Stack } from "@chakra-ui/react";
 
-const Home: NextPage = () => {
+import { Input } from "../components/Form/Input";
+import { Logo } from "../components/Header/Logo";
+
+export default function Home() {
   return (
-    <h1>Helo World!!</h1>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      align="center" 
+      justify="center"
+    > 
+
+      <Flex 
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bgColor="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Box align="center" mb="8">
+          <Logo />
+        </Box>
+        
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+      
+    </Flex>
   )
 }
-
-export default Home
